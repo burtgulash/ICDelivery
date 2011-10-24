f108560bb7d2127f898b4c4287936d007a32a9e6
gusta <burtgulas@gmail.com>
2011-10-24 12:33:12 +0200

    Simulation core

--- src/simulator/OrderEvent.java
+++ src/simulator/OrderEvent.java
@@ -1,23 +1,18 @@
 package simulator;
 
+import stats.Order;
+
 
 public class OrderEvent extends Event {
 	
 	public final int TYPE = 1;  // ORDER event
-		
-	int cargoTons;
-	int dest;
 
-	
+	public final Order order;
 
 	// TRUCKLOAD event
 	
-	public OrderEvent(int cargoTons,int dest, int priority){
-		this.cargoTons = cargoTons;
-		this.dest = dest;
+	public OrderEvent(Order order) {
+		this.order = order;
 		setPriority(priority);
 	}
-	
-	
-
 }
---
+++ src/truckDepot/Scheduler.java
@@ -0,0 +1,26 @@
+package truckDepot;
+
+import stats.Order;
+
+
+/**
+ * Scheduler interface
+ *
+ * assigns Orders to Trucks, which get sent directly to Dispatcher
+ * Tries to minimize transportation costs
+ *
+ * Implemented by GreedyScheduler and ClarkeWrightScheduler
+ *
+ */
+public interface Scheduler {
+
+    /**
+     * Receives Order from Simulator class and sends response to Dispatcher
+     */
+    public void receiveOrder(Order order);
+
+    /**
+     * Forces the scheduler to release every car in its wait queue
+     */
+    public void forceDispatch();
+}
