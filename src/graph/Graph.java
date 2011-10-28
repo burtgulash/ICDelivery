1392dad954cb3384ade7381d5988d0e5ec95a429
gusta <burtgulas@gmail.com>
2011-10-28 17:10:19 +0200

    Graph redesigned

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,45 +1,73 @@
 package graph;
 
+/**
+ * Class graph
+ *
+ * Stored as unidirectional graph but responds to queries as if it was
+ * bidirectional.
+ */
 public class Graph {
-	private Edge[][] v;
-	
-	
-	/**
-	 *  Constructs graph given Edge matrix
-	 */
-	public Graph(Edge[][] v) {
-		this.v = v;
-	}
-	
+    Edge[] v;
+    
+    
+    /**
+     *  Constructs graph with numVertices vertices
+     */
+    public Graph(int numVertices) {
+        v = new Edge[numVertices];
 
-	/**
-	 * Number of vertices of graph
-	 */
-	public int vertices() {
-		return v.length;
-	}
+        // add dummy edges
+        for (int i = 0; i < v.length; i++)
+            v[i] = new Edge(i, -1);
+    }
+    
+    
+    /**
+     * Adds edge from src to dst with given weight
+     */
+    void addEdge(int src, int dst, int weight) {
+        Edge dummy = v[src];
+        Edge inserted = new Edge(dst, weight);
+        inserted.next = dummy.next;
+        dummy.next = inserted;
+    }
+    
+
+    /**
+     * Number of vertices of graph
+     */
+    public int vertices() {
+        return v.length;
+    }
 
 
-	/**
-	 * Returns weight from src to dst or +inf, if no edge
-	 */
-	int cost (int src, int dst) {
-		assert(v[src] != null);
-		int srcListLen = v[src].length;
-		for (int i = 0; i < srcListLen; i++) {
-			assert(v[src][i] != null);
-			if (v[src][i].destination == dst)
-				return v[src][i].weight;
-		}
+    /**
+     * Returns weight from src to dst or +inf, if no edge
+     */
+    int cost (int src, int dst) {
+        for (Edge iter = v[src].next; iter != null; iter = iter.next)
+            if (iter.destination == dst)
+                return iter.weight;
 
-		// else try the edge in other direction
-		assert(v[dst] != null);
-		srcListLen = v[dst].length;
-		for (int i = 0; i < srcListLen; i++) {
-			assert(v[dst][i] != null);
-			if (v[dst][i].destination == src)
-				return v[dst][i].weight;
-		}
-		return Integer.MAX_VALUE;
-	}
+        for (Edge iter = v[dst].next; iter != null; iter = iter.next)
+            if (iter.destination == src)
+                return iter.weight;
+        
+        // if no edge found, return +INFINITY
+        return Integer.MAX_VALUE;
+    }
+
+
+    /**
+     * Little data structure for graph edge
+     */
+    private class Edge {
+        int destination, weight;
+        Edge next;
+
+        Edge(int dst, int w) {
+            destination = dst;
+            weight = w;
+        }
+    }
 }
--- src/graph/GraphLoader.java
+++ src/graph/GraphLoader.java
@@ -6,105 +6,126 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
+/**
+ * Graph Loader class
+ *
+ * Reads graph from file and returns result as parsed data structure
+ *
+ *
+ * Graph format:
+ *      <line>       ::= <number> "{" <edge-list> "}" "\n"
+ *      <edge-list>  ::= <edge> ";" <edge-list> | ""
+ *      <edge>       ::= <number> ":" <number>
+ *      <number>     ::= "[0-9]+"
+ *
+ *
+ */
 public class GraphLoader {
-	private static File graphFile;
-	private static BufferedReader br;
-	private final static int MAX_EDGES = 500;
-	private static Edge[][] v;
-	
-	
-	
-	/**
-	 * 
-	 * Loads graph definition from CSS-like formated file, 
-	 * @throws IOException
-	 */
-	
-	private static List<String> loadGraphFile() throws IOException{
-		List<String> nodeList = new ArrayList<String>();
-		String s;	
-		
-		while ((s = br.readLine()) != null){
-			nodeList.add(s);
-		}	
-		
-		return nodeList;	
-	}
-	
+    
+    /**
+     * Reads graph from file
+     * @return parsed Graph data structure
+     */
+    public static Graph getGraph(String fileName){
+        File graphFile = new File(fileName);
+        assert(graphFile.canRead());
+        BufferedReader reader = null;
+        Graph graph = null;
 
-	
-	private static void parseGraphFile(List<String> nodeList){
-		
-		v = new Edge[nodeList.size()][];
-		Iterator<String> i = nodeList.iterator();
-		Pattern vertex = Pattern.compile("\\d+\\s");
-		Matcher mVertex;
-		int vertexNumber = 0;
-		String s;		
-		
-				
-		while (i.hasNext()){
-			s = i.next();
-			mVertex = vertex.matcher(s);
-						
-			if (mVertex.find())
-				vertexNumber = Integer.parseInt(mVertex.group().trim());
-					
-			if (vertexNumber < v.length){
-				v[vertexNumber] = parseEdgeList(s);
-			} else
-				throw new MalformedGraphFileException();
-					
-		}
-		
-		
-	}
-	
-	private static Edge parseEdge(String s){
-		String[] tmp = s.split(":");
-		return new Edge(Integer.parseInt(tmp[0]),Integer.parseInt(tmp[1]));
-	}	
-	
-	private static Edge[] parseEdgeList(String s){
-		Edge [] edgeList = new Edge[MAX_EDGES];
-		Pattern edges = Pattern.compile("\\d+:\\d+");
-		Matcher mEdges = edges.matcher(s);
-		int count = 0;
-		
-		while (mEdges.find()){
-			edgeList[count++] = parseEdge(mEdges.group());
-		}
-				
-		return Arrays.copyOf(edgeList, count);
-	}
-		
-	
-	public static Graph getGraph(String fileName){
-		graphFile = new File(fileName);
-		assert(graphFile.canRead());
+        try {
+            reader = new BufferedReader(new FileReader(graphFile));
+            graph = parseGraphFile(reader, loadGraphFile(reader));
+        } catch (IOException e) {
+            System.err.println("Error, couldn't read graph file!");
+            graph = null;
+        } catch (MalformedGraphFileException ex) {
+            System.err.println("File is not graph");
+            graph = null;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.err.println("Error closing file");
+                }
+            }
+        }
 
-		try {
-			br = new BufferedReader(new FileReader(graphFile));
-			parseGraphFile(loadGraphFile());
-			br.close();
-		} catch (IOException e) {
-			System.err.println("Error, couldn't read graph file!");
-			return null;
-		}
-		return new Graph(v);	
-	}
-	
+        return graph;
+    }
+
+
+    /**
+     * Loads graph definition from CSS-like formated file, 
+     * @throws IOException
+     */
+    private static List<String> loadGraphFile(BufferedReader reader) 
+                                                          throws IOException
+    {
+        List<String> nodeList = new ArrayList<String>();
+        String s;    
+        
+        while ((s = reader.readLine()) != null)
+            nodeList.add(s);
+        
+        return nodeList;    
+    }
+    
+
+    
+    private static Graph parseGraphFile(BufferedReader reader, 
+                                        List<String> nodeList)
+    {
+        Pattern vertexPattern = Pattern.compile("\\d+\\s*");
+        Matcher mVertex;
+        int vertexNumber = 0;
+        int numVertices  = nodeList.size();
+        
+        Graph graph = new Graph(numVertices);
+                
+        for (String line : nodeList) {
+            mVertex = vertexPattern.matcher(line);
+                        
+            if (mVertex.find()) {
+                try {
+                    vertexNumber = Integer.parseInt(mVertex.group().trim());
+                } catch(NumberFormatException ex) {
+                    throw new MalformedGraphFileException();
+                }
+            }
+
+            if (vertexNumber < numVertices)
+                addEdges(graph, vertexNumber, line);
+            else
+                throw new MalformedGraphFileException();
+        }
+        return graph;
+    }
+
+    // Add edges from src vertex into graph 
+    private static void addEdges(Graph graph, int src, String line) {
+        Pattern edgePattern = Pattern.compile("(\\d+):(\\d+)");
+        Matcher edgeMatcher = edgePattern.matcher(line);
+
+        try {
+            while (edgeMatcher.find()) {
+                int dst       = Integer.parseInt(edgeMatcher.group(1));
+                int weight    = Integer.parseInt(edgeMatcher.group(2));
+                graph.addEdge(src, dst, weight);
+            }
+        } catch (NumberFormatException ex) {
+            throw new MalformedGraphFileException();
+        }
+    }
 }
 
 class MalformedGraphFileException extends RuntimeException {
-	public MalformedGraphFileException() {
-		super();
-	}
+    public MalformedGraphFileException() {
+        super();
+    }
 }
