d9042f766581a1e2e08a84cb595408a997f4b2bc
gusta <burtgulas@gmail.com>
2011-11-02 12:00:48 +0100

    refactored parser

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,46 @@ import java.util.regex.Matcher;
 public class Main {
 
     public static void main(String[] args) {
+        Parser p = new Parser(
+           "Pan Zmrzlik, syn a vnukove - diskretni simulace rozvozu zmrzliny", 
+           "usage:");
+
+        p.addBooleanOption("quiet", "q", "don't print to screen");
+        p.addIntegerOption("time", "t", "total time of simulation", 7200);
+        p.addIntegerOption("initial", "i", "number of initial orders", 300);
+        p.addIntegerOption("pause", "p", "time of first pause", -1);
+        p.addIntegerOption("mean", "m", "mean of interval between orders", 10);
+        p.addIntegerOption("max", "M", 
+                     "maximal amount that can be ordered by one customer", 5);
+
+        p.addStringOption("output", "o", "output file for log", "log.txt");
+        p.addStringOption("graph", "g", "input file containing graph", 
+                                                       "test.graph");
         try {
-            parse(args);
-        } catch(MissingValueException ex) {
+            p.parse(args);
+        } catch(Parser.MissingValueException ex) {
             System.err.println("Missing value after " + ex.arg);
             System.exit(1);
-        } catch(InvalidArgumentException ex) {
-            System.err.println("Invalid argument: " + ex.invalidArg);
+        } catch(Parser.UnknownArgumentException ex) {
+            System.err.println("Invalid argument: " + ex.arg);
             System.exit(1);
         } catch (NumberFormatException ex) {
-            System.err.println("Argument parsing error");
-            System.exit(1);
-        } catch (IndexOutOfBoundsException ex) {
-            System.err.println("Missing argument");
+            System.err.println("Number parsing error");
             System.exit(1);
         }
+
+
+        int HOME = 0;
+
+        boolean quiet        = (Boolean) p.getValue("quiet");
+        int simTime          = (Integer) p.getValue("time");
+        int pauseTime        = (Integer) p.getValue("pause"); 
+        int startOrderCount  = (Integer) p.getValue("initial"); 
+        int orderMean        = (Integer) p.getValue("mean");
+        int maxTonsPerOrder  = (Integer) p.getValue("max");
+        String graphFile     = (String) p.getValue("graph");
+        String outFile       = (String) p.getValue("output");
+
 
         if (pauseTime < 0)
             pauseTime = simTime + 1;
@@ -52,200 +77,9 @@ public class Main {
                                    orderMean,
                                    startOrderCount,
                                    maxTonsPerOrder,
-                                   beQuiet,
+                                   quiet,
                                    file);
 
         Simulator.mainLoop();
-    }
-
-
-
-
-    static final String NL = String.format("%n");
-    static final int HOME = 0;
-
-    static int simTime = 7200;
-    static int pauseTime = -1;
-    static int startOrderCount = 0;
-    static String graphFile = "test.graph";
-    static String outFile = null;
-    static boolean beQuiet = false;
-    static int orderMean  = 10;
-    static int maxTonsPerOrder = 5;
-    
-    public static final String HELP = 
-"Pan Zmrzlik, syn a vnukove - diskretni simulace rozvozu zmrzliny" + NL + NL +
-"Usage: main [OPTIONS...] GRAPHFILE" + NL +
-"  -h | --help        <- print help" + NL + 
-"  -q | --quiet       <- don't print to screen" + NL +
-"  -t | --time        <- total time of simulation" + NL +
-"  -i | --initial     <- number of initial orders" + NL +
-"  -p | --pause       <- time of first pause" + NL +
-"  -o | --output      <- output file for log" + NL +
-"  -g | --graph       <- input  file containing graph" + NL +
-"  -m | --mean        <- mean of interval between orders" + NL +
-"  -M | --max         <- maximal amount that can be ordered by one customer";
-
-    
-    private static int DEPOT = 0;
-
-    static void parse(String[] argv) 
-                     throws IndexOutOfBoundsException, 
-                            NumberFormatException, 
-                            InvalidArgumentException,
-                            MissingValueException
-    {
-        String tmp = "";
-        Matcher m;
-        String help     = "-h|--help";
-        String quiet    = "-q|--quiet";
-        String orders   = "(?:-i|--initial=)(\\d*)|--initial";
-        String time     = "(?:-t|--time=)(\\d*)|--time";
-        String pause    = "(?:-p|--pause=)(\\d*)|--pause";
-        String mean     = "(?:-m|--mean=)(\\d*)|--mean";
-        String max      = "(?:-M|--max=)(\\d*)|--max";
-        String out      = "--output=(\\S+)|-o|--output";
-        String graph    = "--graph=(\\S+)|-g|--graph";
-
-
-        for (int i = 0; i < argv.length; i++) {
-            if (!argv[i].startsWith("-")) {
-                graphFile = argv[i];
-            } 
-            else {
-                if (argv[i].matches(help)) {
-                    System.out.println(HELP);
-                    System.exit(1);
-                }
-
-                else if (argv[i].matches(orders)) {
-                    m = Pattern.compile(orders).matcher(argv[i]);
-                    m.find();
-
-                    tmp = m.group(1);
-                    if (tmp == null || tmp.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        tmp = argv[++i]; 
-                    }
-                    startOrderCount = Integer.parseInt(tmp);
-                }
-
-                else if (argv[i].matches(mean)) {
-                    m = Pattern.compile(mean).matcher(argv[i]);
-                    m.find();
-
-                    tmp = m.group(1);
-                    if (tmp == null || tmp.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        tmp = argv[++i];
-                    }
-                    orderMean = Integer.parseInt(tmp);
-                }
-
-                else if (argv[i].matches(max)) {
-                    m = Pattern.compile(max).matcher(argv[i]);
-                    m.find();
-
-                    tmp = m.group(1);
-                    if (tmp == null || tmp.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        tmp = argv[++i];
-                    }
-                    maxTonsPerOrder = Integer.parseInt(tmp);
-                }
-
-                else if (argv[i].matches(pause)) {
-                    m = Pattern.compile(pause).matcher(argv[i]);
-                    m.find();
-
-                    tmp = m.group(1);
-                    if (tmp == null || tmp.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        tmp = argv[++i];
-                    }
-                    pauseTime = Integer.parseInt(tmp);
-                }
-
-                else if (argv[i].matches(time)) {
-                    m = Pattern.compile(time).matcher(argv[i]);
-                    m.find();
-
-                    tmp = m.group(1);
-                    if (tmp == null || tmp.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        tmp = argv[++i];
-                    }
-                    simTime = Integer.parseInt(tmp);
-                }
-
-                else if (argv[i].matches(quiet)) {
-                    beQuiet = true;
-                }
-
-                else if (argv[i].matches(out)) {
-                    m = Pattern.compile(out).matcher(argv[i]);
-                    m.find();
-
-                    outFile = m.group(1);
-                    if (outFile == null || outFile.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        outFile = argv[++i];
-                    }
-                }
-
-                else if (argv[i].matches(graph)) {
-                    m = Pattern.compile(graph).matcher(argv[i]);
-                    m.find();
-
-                    graphFile = m.group(1);
-                    if (graphFile == null || graphFile.equals("")) {
-                        if (argv[i].endsWith("="))
-                            throw new MissingValueException(argv[i]);
-                        graphFile = argv[++i];
-                    }
-                }
-
-                else {
-                    throw new InvalidArgumentException(argv[i]);
-                }
-            }
-        }
-    }
-
-    // debugging
-    private static void printConfig() {
-        System.out.println("quiet    :   " + beQuiet);    
-        System.out.println("time     :   " + simTime);    
-        System.out.println("initial  :   " + startOrderCount);    
-        System.out.println("pause    :   " + pauseTime);    
-        System.out.println("output   :   " + outFile);    
-        System.out.println("graph    :   " + graphFile);    
-        System.out.println("mean     :   " + orderMean);    
-        System.out.println("max      :   " + maxTonsPerOrder);    
-    }
-
-
-    
-}
-
-class MissingValueException extends Exception {
-    final String arg;
-    MissingValueException(String arg) {
-        super();
-        this.arg = arg;
-    }
-}
-
-class InvalidArgumentException extends Exception {
-    final String invalidArg;
-    InvalidArgumentException(String arg) {
-        super();
-        invalidArg = arg;
     }
 }
---
+++ src/Parser.java
@@ -0,0 +1,334 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Command line options parser
+ *
+ * 1) specify string, boolean or integer options
+ * 2) parse String[] args
+ * 3) retrieve options and left over arguments
+ * 4) profit.
+ *
+ * Don't use -h or --help for String option.
+ */
+public class Parser {
+
+    private Map<String, Object> values;
+    private List<Option> options;
+    private String chapeau;
+
+
+
+    /**
+     * Constructs new Parser object.
+     *
+     * @param title to be printed in the beginning of help string
+     * @param usage usage string
+     */
+    public Parser(String title, String usage) {
+        values   = new HashMap<String, Object>();
+        options  = new LinkedList<Option>();
+        chapeau  = title;
+
+        options.add(new HelpOption());
+    }
+
+    /**
+     * Constructs new Parser object.
+     */
+    public Parser() {
+        this(null, "usage:");
+    }
+
+    
+    /**
+     * Prints help.
+     */
+    private void printHelp() {
+        if (chapeau != null) {
+            System.err.println(chapeau);
+            System.err.println();
+        }
+
+        System.err.println("usage:");
+        for (Option option : options)
+            System.err.println(String.format("    -%s | --%s \t\t%s",
+                                  option.abbr, option.name, option.help));
+    }
+
+
+    /**
+     * Retrieve parsed value for given option.
+     * Should be called after parsing.
+     *
+     * @param name name of an option
+     * @return Parsed value or null if it wasn't specified 
+     *         or name doesn't exist.
+     */
+    public Object getValue(String name) {
+        return values.get(name);
+    }
+
+
+    /**
+     * Parse previously specified options and returns non-option arguments.
+     *
+     * @param args Command line arguments
+     * @return Non optional arguments.
+     * @throws MissingValueException if there was no value to an option.
+     * @throws UnknownArgumentException if an argument was an option but was
+     *                                  not specified.
+     * @throws NumberFormatException if an integer option could not be parsed.
+     */
+    public String[] parse(String[] args) 
+          throws MissingValueException, UnknownArgumentException,
+                 NumberFormatException 
+    {
+        List<String> leftOvers = new LinkedList<String>();
+        
+        outer:
+        for (int i = 0; i < args.length; i++) {
+            // if it is an option
+            if (args[i].startsWith("-")) {
+                for (Option option : options) {
+                    if (option.matches(args[i])) {
+                        i = option.addValue(args, i);
+                        continue outer;
+                    }
+                }
+                throw new UnknownArgumentException(args[i]);
+            // else non-option
+            } else {
+                leftOvers.add(args[i]);
+            }
+        }
+
+        return leftOvers.toArray(new String[leftOvers.size()]);
+    }
+
+
+    /**
+     * Specifies integer option
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     */
+    public void 
+    addIntegerOption(String longName, String shortName, String help) {
+        addIntegerOption(longName, shortName, help, 0);
+    }
+
+    /**
+     * Specifies integer option
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     * @param defaultValue value if the option was not present
+     */
+    public void 
+    addIntegerOption(String longName, String shortName,
+                     String help, int defaultValue) {
+        String regex =
+           String.format("(?:-%s|--%s=)(\\d*)|--%s", 
+                          shortName, longName, longName);
+
+        options.add(new IntegerOption(longName, shortName, regex, help));
+        values.put(longName, defaultValue);
+    }
+
+
+
+    /**
+     * Specifies string option
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     */
+    public void 
+    addStringOption(String longName, String shortName, String help) {
+        addStringOption(longName, shortName, help, null);
+    }
+
+
+    /**
+     * Specifies string option
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     * @param defaultValue value if the option was not present
+     */
+    public void 
+    addStringOption(String longName, String shortName, 
+                    String help, String defaultValue)
+    {
+        String regex =
+           String.format("--%s=(\\S+)|-%s|--%s", longName, shortName, longName);
+
+        options.add(new StringOption(longName, shortName, regex, help));
+        values.put(longName, defaultValue);
+    }
+    
+
+
+    /**
+     * Specifies flag
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     */
+    public void 
+    addBooleanOption(String longName, String shortName, String help) {
+        addBooleanOption(longName, shortName, help, false);
+    }
+
+
+    /**
+     * Specifies flag
+     *
+     * @param longName full name of the option
+     * @param shortName should be one character identifier of the option
+     * @param help help message
+     * @param defaultValue value if the option was not present
+     */
+    public void 
+    addBooleanOption(String longName, String shortName, 
+                     String help, boolean defaultValue) 
+    {
+        String regex = String.format("-%s|--%s", shortName, longName);
+        options.add(new BooleanOption(longName, shortName, regex, help));
+        values.put(longName, defaultValue);
+    }
+
+
+
+
+
+    private abstract class Option {
+        String name, abbr, regex, help;
+
+        Option (String name, String abbr, String regex, String help) {
+            this.name   = name;
+            this.abbr   = abbr;
+            this.regex  = regex;
+            this.help   = help;
+        }
+
+        final boolean matches(String arg) {
+            return Pattern.matches(regex, arg);
+        }
+
+        abstract int addValue(String[] args, int i) 
+                     throws MissingValueException ;
+    }
+
+
+    private class StringOption extends Option {
+        StringOption(String name, String abbr, String regex, String help) {
+            super(name, abbr, regex, help);
+        }
+
+        @Override
+        int addValue(String[] args, int i) throws MissingValueException {
+            Matcher m = Pattern.compile(regex).matcher(args[i]);
+            m.find();
+
+            String tmp = m.group(1);
+            if (tmp == null || tmp.equals("")) {
+                if (args[i].endsWith("=") || i + 1 >= args.length)
+                    throw new MissingValueException(args[i]);
+                tmp = args[++i];
+            }
+            values.put(name, tmp);
+
+            return i;
+        }
+    }
+
+
+    private class IntegerOption extends Option {
+        IntegerOption(String name, String abbr, String regex, String help) {
+            super(name, abbr, regex, help);
+        }
+
+        @Override
+        int addValue(String[] args, int i) 
+                     throws MissingValueException, NumberFormatException 
+        {
+            Matcher m = Pattern.compile(regex).matcher(args[i]);
+            m.find();
+
+            String tmp = m.group(1);
+            if (tmp == null || tmp.equals("")) {
+                if (args[i].endsWith("=") || i + 1 >= args.length)
+                    throw new MissingValueException(args[i]);
+                tmp = args[++i];
+            }
+
+            Integer parsed = Integer.valueOf(tmp);
+            values.put(name, parsed);
+
+            return i;
+        }
+    }
+
+
+    private class BooleanOption extends Option {
+        BooleanOption(String name, String abbr, String regex, String help) {
+            super(name, abbr, regex, help);
+        }
+
+        @Override
+        int addValue(String[] args, int i) {
+            values.put(name, true);
+            return i;
+        }
+    }
+
+    private class HelpOption extends Option {
+        HelpOption() {
+            super("help", "h", "-h|--help", "shows help");
+        }
+
+        @Override
+         int addValue(String[] args, int i) {
+            printHelp();
+            System.exit(1);
+            return i;
+        }
+    }
+
+
+
+
+    /**
+     * Thrown when there is no value to an option but should be present.
+     */
+    public static class MissingValueException extends Exception {
+        final String arg;
+        MissingValueException(String arg) {
+            super();
+            this.arg = arg;
+        }
+    }
+
+    /**
+     * Thrown if argument is an option but not recognized.
+     */
+    public static class UnknownArgumentException extends Exception {
+        final String arg;
+        UnknownArgumentException(String arg) {
+            super();
+            this.arg = arg;
+        }
+    }
+}
