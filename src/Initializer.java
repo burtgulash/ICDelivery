b999e8932f8f261a66b5b0ebc15feb1c6e2d738a
gusta <burtgulas@gmail.com>
2011-11-13 17:21:46 +0100

    Initializer javadoc

--- src/Initializer.java
+++ src/Initializer.java
@@ -8,11 +8,33 @@ import graph.Graph;
 
 
 /**
+ * <p>
  * Takes care of initialization of simulator components in correct order.
+ * </p>
+ *
+ * @author Tomas Marsalek
  */
 public class Initializer {
+
     /**
-     * Initializes simulation from given parameters
+     * Initializes simulation with given parameters
+     *
+     * @param graph Town-map represented as Graph data structure
+     * @param homeVertex determine which one of graph's vertices will be 
+     *                   HOME/DEPOT town
+     * @param simulationTime time in minutes the simulation will terminate after
+     * @param pause time in minutes of first interactive pause
+     * @param orderMean mean value of length of time interval between 
+     *                  generated orders
+     * @param startOrders amount of initial orders (generated at 00:00:00)
+     * @param orders list of manually entered orders
+     * @param maxOrderAmount maximum amount a customer can order per one order
+     * @param quiet true - don't print log to stdout, false - be verbose
+     * @param logFile file to output log of simulation
+     * @param reportFiles array of 3 report files 
+     *                    (customer, order, truck) in this order
+     * @param strategy scheduler strategy to be used in simulation,
+     *                 supported: greedy, clarkewright
      */
     public static void initSimulation (Graph graph, int homeVertex, 
                           int simulationTime, String pause, int orderMean, 
@@ -52,9 +74,15 @@ public class Initializer {
 
         OrderGenerator gen = new ExponentialGenerator(orderMean, 
                                                   maxOrderAmount, homeVertex);
-        generateOrders(gen, simulationTime, startOrders, orderMean);
+        generateOrders(gen, simulationTime, startOrders);
     }
 
+
+    /**
+     * Adds first pause event to Calendar.
+     *
+     * @param pause time of first interactive pause
+     */
     private static void addFirstPause(String pause) {
         if (pause != null) {
             int pauseTime = 
@@ -68,25 +96,43 @@ public class Initializer {
     }
 
 
+    /**
+     * Generate initially known orders (not those manually provided by user)
+     *
+     * @param gen order generator implementation chosen to generate orders
+     * @param simulationTime generator needs to know, when to stop generating
+     *                       orders
+     * @param startOrders number of orders to automatically generate at 00:00:00
+     */
     private static void generateOrders(OrderGenerator gen, int simulationTime, 
-                                       int startOrders, int mean) 
+                                       int startOrders) 
     {
         int HALF_DAY   = DAY.time() / 2;
 
         Order generated;
         for (int i = 0; i < startOrders; i++) {
             generated = gen.generateAt(Simulator.START_TIME);
-            sendOrder(generated);
+
+            Event orderEvent = new OrderEvent(generated.received(), generated);
+            Calendar.addEvent(orderEvent);
         }
 
         while (true) {
             generated = gen.generateNext();
             if (generated.received() >= simulationTime - HALF_DAY)
                 break;
-            sendOrder(generated);
+
+            Event orderEvent = new OrderEvent(generated.received(), generated);
+            Calendar.addEvent(orderEvent);
         }
     }
 
+
+    /**
+     * Parse and insert all initially known manually specified orders.
+     *
+     * @param orders list of all manually specified orders
+     */
     private static void addOrders(String[] orders) {
         for (String orderString : orders) {
             StringTokenizer st = new StringTokenizer(orderString, ",");
@@ -114,11 +160,5 @@ public class Initializer {
             Event orderEvent = new OrderEvent(time, o);
             Calendar.addEvent(orderEvent);
         }
-    }
-
-
-    private static void sendOrder(Order order) {
-        Event orderEvent = new OrderEvent(order.received(), order);
-        Calendar.addEvent(orderEvent);
     }
 }
